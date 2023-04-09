d21b47b7998306e37b259b5a8097a468e752978f
Tim Eitel <timeitel@outlook.com>
2023-04-09 20:56:39 +1000

    chore: scaffolding player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod gameplay;
+// use game::GamePlugin;
+// use main_menu::MainMenuPlugin;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
+        // .add_plugin(MainMenuPlugin)
+        // .add_plugin(GamePlugin)
+        .run();
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Gameplay,
+    GameOver,
 }
