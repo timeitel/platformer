474da62cf81d2321ecf1340123855a23bebefa0f
Tim Eitel <timeitel@outlook.com>
2023-05-14 20:38:08 +1000

    rf: remove old refs

--- src/gameplay/player/systems.rs
+++ src/gameplay/player/systems.rs
@@ -1,7 +1,7 @@
-use crate::gameplay::animation::components::*;
-
 use super::components::Player;
 use bevy::prelude::*;
+
+const MOVE_SPEED: f32 = 100.;
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -16,26 +16,7 @@ pub fn spawn_player(
         None,
         None,
     );
-
-    commands.spawn((
-        SpriteSheetBundle {
-            texture_atlas: texture_atlas.add(atlas),
-            sprite: TextureAtlasSprite {
-                index: 0,
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-        Player,
-        SpriteAnimation {
-            len: 11,
-            frame_time: 1. / 10.,
-        },
-        FrameTime(0.0),
-    ));
 }
-
-const MOVE_SPEED: f32 = 100.;
 
 pub fn move_player(
     mut player: Query<&mut Transform, With<Player>>,
@@ -47,35 +28,5 @@ pub fn move_player(
         transform.translation.x -= MOVE_SPEED * time.delta_seconds();
     } else if input.any_pressed([KeyCode::D, KeyCode::Right]) {
         transform.translation.x += MOVE_SPEED * time.delta_seconds();
-    }
-}
-
-pub fn change_player_animation(
-    mut player: Query<
-        (
-            &mut Handle<TextureAtlas>,
-            &mut SpriteAnimation,
-            &mut TextureAtlasSprite,
-        ),
-        With<Player>,
-    >,
-    input: Res<Input<KeyCode>>,
-    mut texture_atlas: ResMut<Assets<TextureAtlas>>,
-    asset_server: Res<AssetServer>,
-) {
-    let (mut atlas, mut animation, mut sprite) = player.single_mut();
-
-    let left_keys = [KeyCode::Left, KeyCode::A];
-    let right_keys = [KeyCode::Right, KeyCode::D];
-
-    if input.any_just_pressed(left_keys) {
-        sprite.flip_x = true;
-    } else if input.any_just_pressed(right_keys) && !input.any_pressed(left_keys) {
-        sprite.flip_x = false;
-    } else if input.any_just_released(left_keys)
-        && !input.any_pressed(left_keys)
-        && input.any_pressed(right_keys)
-    {
-        sprite.flip_x = false;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 mod gameplay;
-// use game::GamePlugin;
-// use main_menu::MainMenuPlugin;
 
 use bevy::prelude::*;
-use gameplay::animation::systems::*;
 use gameplay::player::systems::*;
 
 fn main() {
@@ -12,9 +9,7 @@ fn main() {
         .add_state::<AppState>()
         // .add_plugin(MainMenuPlugin)
         // .add_plugin(GamePlugin)
-        .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
-        .add_system(animate_sprite)
         .add_system(move_player)
         .run();
 }
@@ -25,8 +20,4 @@ pub enum AppState {
     MainMenu,
     Gameplay,
     GameOver,
-}
-
-fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
 }
